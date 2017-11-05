6b4b9f2a2c0a0a6095345af524d89a492d319dee
yutakasi634 <p7dea87c7e4n7j@gmail.com>
2017-11-05 19:32:26 +0900

    add comment about Meals table.

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,3 +1,9 @@
 class Meal < ApplicationRecord
+  # ======= description about Meal table schema ===========
+  # time_zone_num column is correspond to time_zone
+  # breakfirst = 0
+  # lunch = 1
+  # dinner = 2
+  # ===============================================
   scope :todays_meal, -> { where(date: Time.now.to_date) }
 end
