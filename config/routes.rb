795ace98c5884270ff52ab4bd6890f473b456c67
yutakasi634 <p7dea87c7e4n7j@gmail.com>
2017-11-05 18:46:00 +0900

    set route to top page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :records
   resources :meals
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'top_page', to: 'meals#top_page'
 end
