bfb68b2d319a8f29251e155498c58d71422b5d69
yutakasi634 <p7dea87c7e4n7j@gmail.com>
2017-12-15 01:19:49 +0900

    toppe-zi

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :records
   resources :meals
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'top_page', to: 'meals#top_page'
+  get 'top', to: 'meals#top'
 end
